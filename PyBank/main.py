14dd05692b424bd81ecfde8affd68e5fe80bf9b5
sushmamylavarapu <sushmamylavarapu1@gmail.com>
2023-05-24 20:39:18 -0400

    Pushing updated files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,56 +1,72 @@
 import os
 import csv
-filename='budget_data.csv'
-budget_data = os.path.join("Resources","budget_data.csv")
-#Variables
-total=0
-date=[]
-months=0
-total_months=0
-monthly_profit_losses=[]
-increase=["",0]
-decrease=["",0]
-change=0
-total_change_profit_losses=0
-previous_revenue=0
+
+filename = 'budget_data.csv'
+budget_data = os.path.join("budget_data.csv")
+pathout = os.path.join("Analysis", "budget_data.txt")
+
+# Variables
+total_months = 0
+total_profit_losses = 0
+monthly_profit_losses = []
+change_profit_losses = []
+date = []
+
 with open(budget_data, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    csv_header=next(csvreader)
-    #Read rows of data starting next to header: 
+    csv_header = next(csvreader)
+    previous_profit_losses = 0
     for row in csvreader:
-        date.append(row[0])
-        monthly_profit_losses.append(row[1])
-        #Total months:
-        months+=1
-        total_months+= months+int(row[1])
-        #Total Change:
-        change=int(row[1])-previous_revenue
-        if previous_revenue==0:
-            change=0
-            previous_revenue=int(row[1])
-            total_change_profit_losses += change
-            #Evaluate Increase
-            if change > int(increase[1]):
-                increase[1]=change
-                increase[0]=row[0]
-                #Evaluate Decrease
-                if change<int(decrease[1]):
-                    decrease[1]=change
-                    decrease[0]=row[0]
-                    total_change_profit_losses=total_change_profit_losses/(months-1)
-               
+        
+        # Total months:
+        total_months += 1
 
-    # Profit_losses
-    # change_profit_losses = int(row[1]) - current_profit_losses
-    # current_profit_losses = int(row[1])
-    # total = total + current_profit_losses
-      
-    print(f'Financial Analysis')
-    print(f'-----------------------------')
-    print(f'Total months: {total_months}')
-    print(f'Total: {total_change_profit_losses}')
-    print(f'Average change: ${change:.2f}')
-    print(f'Greatest Increase in Profits: {increase}')
-    print(f'Greatest Decrease in Profits: {decrease}')
+        # Total profit/losses:
+        current_profit_losses = int(row[1])
+        total_profit_losses += current_profit_losses
 
-   
+        # list of monthly profit/losses:
+        monthly_profit_losses.append(current_profit_losses)
+        
+        # Total change in profit/losses:
+        if previous_profit_losses != 0:
+         
+            change_profit_losses.append(current_profit_losses - previous_profit_losses)
+            # Updating previous profit/losses:
+            previous_profit_losses = current_profit_losses
+            date.append(row[0])
+        else:
+            previous_profit_losses = current_profit_losses    
+            # Average change:
+
+    #print(change_profit_losses)        
+    average_change = sum(change_profit_losses) / len(change_profit_losses)
+                    
+    # Greatest increase and Greatest decrease in profits/losses:
+    greatest_increase = max(change_profit_losses)
+    greatest_decrease = min(change_profit_losses)
+
+    # Dates for Greatest increase and Greatest decrease in profits/losses:
+    increase_index = change_profit_losses.index(greatest_increase)
+    decrease_index = change_profit_losses.index(greatest_decrease)
+    greatest_increase_date = date[increase_index + 1]
+    greatest_decrease_date = date[decrease_index + 1]
+
+# Financial Analysis:
+print("Financial Analysis")
+print("-----------------------------")
+print(f"Total months: {total_months}")
+print(f"Total: ${total_profit_losses}")
+print(f"Average change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits: {greatest_increase_date} ${greatest_increase}")
+print(f"Greatest Decrease in Profits: {greatest_decrease_date} ${greatest_decrease}")
+
+#Text file with the results 
+with open(pathout, "w") as Analysis:
+    Analysis.write(f"Financial Analysis\n")
+    Analysis.write("-----------------------------\n")
+    Analysis.write(f"Total months: {total_months}\n")
+    Analysis.write(f"Total: ${total_profit_losses}\n")
+    Analysis.write(f"Average change: ${average_change:.2f}\n")
+    Analysis.write(f"Greatest Increase in Profits: {greatest_increase_date} ${greatest_increase}\n")
+    Analysis.write(f"Greatest Decrease in Profits: {greatest_decrease_date} ${greatest_decrease}\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,8 @@
 import os 
 import csv
+import numpy
 
-csvpath = os.path.join("Resources", "election_data.csv")
+csvpath = os.path.join("election_data.csv")
 pathout = os.path.join("Analysis", "election_results.txt")
 
 # Variables to store data
@@ -11,6 +12,9 @@ county=[]
 Charles_Casper_Stockham=[]
 Diana_DeGette=[]
 Raymon_Anthony_Doane=[]
+votes_Charles_Casper_Stockham=0
+votes_Diana_DeGette=0
+votes_Raymon_Anthony_Doane=0
 
 #create three columns: 
 with open(csvpath) as csvfile:
@@ -18,42 +22,47 @@ with open(csvpath) as csvfile:
 	csv_header=next(csvreader)
 
 #Read each row of data after the header and loop through rows
-for row in csvreader:
-	votes.append(row[0])
-	county.append(row[1])
-	candidates.append(row[2])
-
-    # The total number of votes cast   
+	for row in csvreader:
+		votes.append(row[0])
+		county.append(row[1])
+		candidates.append(row[2])
+		
+# The total number of votes cast   
 votes_total=len(row[1])
 
 
 # A complete list of candidates who received votes and the
 for can in candidates:
-	if can=="Charles_Casper_Stockham":
-		Charles_Casper_Stockham.append(candidates)
-		votes_Charles_Casper_Stockham=len (Charles_Casper_Stockham)
-	elif can=="Diana_DeGette":
-		Diana_DeGette.append(candidates)
-		votes_Diana_DeGette=len(Diana_DeGette)
-		can=="Raymon_Anthony_Doane"
-		Raymon_Anthony_Doane.append(candidates)
-		votes_Raymon_Anthony_Doane=len(Raymon_Anthony_Doane)
+	
+	if can=="Charles Casper Stockham":
+		Charles_Casper_Stockham.append(can)
+		
+	elif can=="Diana DeGette":
+		Diana_DeGette.append(can)
+	else:	
+		Raymon_Anthony_Doane.append(can)
+		
+votes_Charles_Casper_Stockham=len (Charles_Casper_Stockham)
+votes_Diana_DeGette=len(Diana_DeGette)
+votes_Raymon_Anthony_Doane=len(Raymon_Anthony_Doane)
 
 # The percentage of votes each candidate won.
-per_Charles_Casper_Stockham=round(((votes_Charles_Casper_Stockham/votes_total)*100), 2) 
-per_Diana_DeGette=round(((votes_Diana_DeGette/votes_total)*100), 2)
-per_Raymon_Anthony_Doane=round(((votes_Raymon_Anthony_Doane/votes_total)*100), 2)
+per_Charles_Casper_Stockham=round(((votes_Charles_Casper_Stockham/len(votes))*100), 2) 
+per_Diana_DeGette=round(((votes_Diana_DeGette/len(votes))*100), 2)
+per_Raymon_Anthony_Doane=round(((votes_Raymon_Anthony_Doane/len(votes))*100), 2)
 
 
 # The winner of the election based on popular vote. 
-def winner (candidates):
-	return max(set (candidates), key=candidates.count)
+candidateList=["Charles_Casper_Stockham", "Diana_DeGette", "Raymon_Anthony_Doane"]
+resultList=[votes_Charles_Casper_Stockham, votes_Diana_DeGette, votes_Raymon_Anthony_Doane]
+index=numpy.argmax(resultList)
+winner=candidateList[index]
 
 # Print the analysis to the terminal.
 
 print(f'Election Results')
 print('------------------')
-print(f'Total votes: {votes}')
+print(f'Total votes: {len(votes)}')
 print(f'-----------------')
 print(f'Charles_Casper_Stockham: %{per_Charles_Casper_Stockham} {votes_Charles_Casper_Stockham}')
 print(f'Diana_DeGette: %{per_Diana_DeGette} {votes_Diana_DeGette}')
@@ -63,14 +72,14 @@ print(f'Winner: {winner}')
 print('-------------------')
 
 # Text file with the results 
-with open(pathout, "w") as results:
-	results.write(f"Election Results\n")
-	results.write("----------------------\n")
-	results.write(f"Total votes: {votes}\n")
-	results.write(f"----------------------\n")
-	results.write(f"Charles_Casper_Stockham: %{per_Charles_Casper_Stockham} ({votes_Charles_Casper_Stockham})\n")
-	results.write(f"Diana_DeGette: %{per_Diana_DeGette} ({votes_Diana_DeGette})\n")
-	results.write(f"Raymon_Anthony_Doane: %{per_Raymon_Anthony_Doane} ({votes_Raymon_Anthony_Doane})\n")
-	results.write("----------------------")
-	results.write(f"Winner: ({winner})\n")
-	results.write(f"----------------------")
+with open(pathout, "w") as election_results:
+	election_results.write(f"Election Results\n")
+	election_results.write("----------------------\n")
+	election_results.write(f"Total votes: {votes}\n")
+	election_results.write(f"----------------------\n")
+	election_results.write(f"Charles_Casper_Stockham: %{per_Charles_Casper_Stockham} ({votes_Charles_Casper_Stockham})\n")
+	election_results.write(f"Diana_DeGette: %{per_Diana_DeGette} ({votes_Diana_DeGette})\n")
+	election_results.write(f"Raymon_Anthony_Doane: %{per_Raymon_Anthony_Doane} ({votes_Raymon_Anthony_Doane})\n")
+	election_results.write("----------------------")
+	election_results.write(f"Winner: ({winner})\n")
+	election_results.write(f"----------------------")
